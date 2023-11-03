8f0360ed9051be5c2ac16f79235ef9742099326d
rishikeshydv <rishikeshadh4@gmail.com>
2023-11-03 00:05:45 -0400

    K_stack added

---
+++ kruetzer/K_stack.py
@@ -0,0 +1,35 @@
+class K_stack:
+    def __init__(self):
+        self.stack = []
+        
+    def push(self,val):
+        if not val:
+            raise ValueError("No input data")
+        self.stack.append(val)
+        
+    def pop(self):
+        self.stack.pop(0)
+    def find(self,val):
+        if not val:
+            raise ValueError("No input data")
+        idx = self.stack.index(val)
+        if idx:
+            return idx
+        else:
+            raise ValueError("No such number found")
+    def minimum(self):
+        return min(self.stack)
+    def maximum(self):
+        return max(self.stack)
+    def display(self):
+        output = "[ " + "  |  ".join(str(element) for element in self.stack) + " ]"
+        print(output)
+    
+    
+sol = K_stack()
+sol.push(5)
+sol.push(6)
+sol.push(7)
+sol.push(8)
+sol.push(9)
+print(sol.display())
